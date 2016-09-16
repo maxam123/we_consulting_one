341a9ddbc6bd881d52f9a62226fb721f8978d4b1
Max desktop <maxam123@hotmail.com>
2016-09-16 09:19:04 +0200

    Adds styling and basic functionality to profile page

--- app/main/controllers/profile-ctrl.js
+++ app/main/controllers/profile-ctrl.js
@@ -1,7 +1,29 @@
 'use strict';
 angular.module('main')
-.controller('ProfileCtrl', function ($log) {
+.controller('ProfileCtrl', function ($log, $ionicPopup, $scope) {
+  $scope.showAlert = function(icon) {
+  $scope.data = icon;
+  switch (icon) {
+    case 'email':
+      $scope.title='maal1207@student.miun.se';
+      $scope.template='redirect to email app';
+      break;
+    case 'phone':
+      $scope.title='+46722013199';
+      $scope.template='redirect to call app';
+      break;
+    case 'video':
+      $scope.title='video conference';
+      $scope.template='redirect to video conference app';
+      break;
+    default:
+      $scope.title='error';
+      $scope.template='error';
 
-  $log.log('Hello from your Controller: ProfileCtrl in module main:. This is your controller:', this);
-
+  }
+  $ionicPopup.alert({
+    title: $scope.title,
+    template: $scope.template
+  });
+  };
 });

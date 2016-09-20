8474a20ac856cc05f699990b2a98e235b04f542b
Thomas Schoutsen <thsc1600@student.miun.se>
2016-09-20 14:05:12 +0200

    Adds news to homepage

--- app/main/controllers/home-ctrl.js
+++ app/main/controllers/home-ctrl.js
@@ -3,9 +3,12 @@
  */
 'use strict';
 angular.module('main')
-  .controller('HomeCtrl', function ($log, $scope) {
+  .controller('HomeCtrl', function ($log, $scope, $ionicSideMenuDelegate) {
 
     $log.log('Hello from your Controller: HomeCtrl in module main:. This is your controller:', this);
+
+    // disable sliding in the sidemenu on this view
+    $ionicSideMenuDelegate.canDragContent(false);
 
     $scope.mails = [
       {
@@ -18,7 +21,24 @@ angular.module('main')
         senderName: 'Daniel Peterson',
         emailText: 'Hej Hej, small update on the meeting we\'re having today. What do you think about doing it tomorrow instead?'
       }
-    ]
+    ];
+
+    $scope.recentNews = [
+      {
+        date: '19/09/2016',
+        image: 'main/assets/images/profile_placeholder_4.jpg',
+        author: 'Reza Perona',
+        title: 'Updates on the Intranet',
+        content: 'From now on the intranet will feature a new way to attend video conferences. This update will make it easier to communicate with your colleagues. Please use it, it\'s awesome! And bla bla bla and woopdiedobiedoo and yahoo this is really cool.'
+      },
+      {
+        date: '11/09/2016',
+        image: 'main/assets/images/profile_placeholder_2.jpg',
+        author: 'Ana Eliasson',
+        title: 'We conference (The Netherlands)',
+        content: 'The Netherlands, a country in northwestern Europe, is known for a flat landscape of canals, tulip fields, windmills and cycling routes. Amsterdam, the capital, is home to the Rijksmuseum, Van Gogh Museum and the house where Jewish diarist Anne Frank hid during WWII. Canalside mansions and a trove of works from artists including Rembrandt and Vermeer remain from the city\'s 17th-century "Golden Age."'
+      }
+    ];
 
     $scope.highscores = [
       {
@@ -46,6 +66,10 @@ angular.module('main')
       {
         eventTime: '15.30 - 17.00',
         eventText: 'Meeting with Daniel'
+      },
+      {
+        eventTime: '18.00 - 20.00',
+        eventText: 'Dinner with client'
       }
     ];
 

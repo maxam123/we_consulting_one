8d8500259d04ce6338235d130b67db0606249769
Max <maxam123@hotmail.com>
2016-09-20 15:14:50 +0200

    Adds search functionality in sidebar

--- app/main/controllers/menu-ctrl.js
+++ app/main/controllers/menu-ctrl.js
@@ -1,7 +1,36 @@
 'use strict';
 angular.module('main')
-.controller('MenuCtrl', function ($log) {
+.controller('MenuCtrl', function ($log, $scope) {
+  $scope.showContent = true;
+  $scope.showResult = false;
 
-  $log.log('Hello from your Controller: MenuCtrl in module main:. This is your controller:', this);
+  $scope.menu = {
+    search: '',
+    lastSearch: ''
+  };
 
+  $scope.onSearchChange = function() {
+    $log.log($scope.menu.search.length);
+    if($scope.menu.search.length > 0){
+      $scope.showContent = false;
+      $scope.showResult = true;
+      $log.log("greater");
+    }else{
+      $scope.showContent = true;
+      $scope.showResult = false;
+    }
+  };
+  $scope.discardSearch = function() {
+
+    $scope.menu.lastSearch = $scope.menu.search;
+    $scope.menu.search = "";
+    $scope.showContent = true;
+    $scope.showResult = false;
+  };
+  $scope.clickLastSearch = function() {
+    $scope.menu.lastSearch = $scope.menu.search;
+    $scope.menu.search = "";
+    $scope.showContent = true;
+    $scope.showResult = false;
+  };
 });
